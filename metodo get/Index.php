be42908abe503327c545cd4c1a12bd232fd82468
Rafael Felix De Maria <rafael.maria01@etec.sp.gov.br>
2025-04-11 12:19:52 -0300

    Exercício GET php

---
+++ metodo get/Index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Exemplo</title>
+    </head>
+    <body>
+
+        <?php
+            $ano=$_GET["ano"];
+            echo "o ano atual é $ano";
+/*
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Exemplo</title>
+</head>
+<body>
+    <?php
+    $ano = date("Y");
+    echo "O ano atual é $ano";
+    $anoanterior = $ano - 1;
+    echo "<br>O ano anterior é $anoanterior";
+    $anosucessor = $ano + 1 ;
+    echo "<br>O ano sucessor é $anosucessor";
+    ?>
+</body>
+</html>
+
+metodo mais fácil
+*/
+        ?>
+    </body>
+</html>
+
