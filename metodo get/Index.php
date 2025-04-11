f13c1a3c62a91aec4233bf3de87f00bdf5cd2698
Rafael Felix De Maria <rafael.maria01@etec.sp.gov.br>
2025-04-11 12:23:53 -0300

    Finalização do exercício

--- metodo get/Index.php
+++ metodo get/Index.php
@@ -8,6 +8,10 @@
         <?php
             $ano=$_GET["ano"];
             echo "o ano atual é $ano";
+            $anoanterior=$ano - 1;
+            echo "<br> o ano anterior é $anoanterior";
+            $anosucessor=$ano + 1;
+            echo "<br> o ano sucessor é $anosucessor";
 /*
 <!DOCTYPE html>
 <html>
